07c7135b149ee709a781eece1d2ce6fda349b30b
JackJack2405 <chutikankin7442@gmail.com>
2025-11-20 13:09:22 +0700

    task:ts

--- task04.ts
+++ task04.ts
@@ -6,13 +6,28 @@
 
 // TODO BONUS: Rewrite with if
 
-const num = 15;
+const num = 9;
 if (num % 3 === 0 && num % 5 === 0){
     console.log("FizzBuzz");
 } else if (num % 3 === 0){
     console.log("Fizz")
-}else if(num %5 ===0){
+}else if(num %5 === 0){
     console.log("Buzz")
 }else{
     console.log("Not divisible by 3 or 5")
+}
+
+
+// TODO BONUS: Rewrite with if
+let number = 37;
+if(number === 0){
+  console.log("Zero is not acceptable.")
+} else if ((number % 3 === 0) && (number % 5 === 0)) {
+  console.log(`Your number:${number} is "FizzBuzz".`);
+} else if ((number % 3 === 0) ) {
+    console.log(`Your number:${number} is "Fizz".`);
+} else if ((num % 5 === 0) ) {
+    console.log(`Your number:${number} is "Buzz".`);
+} else {
+  console.log("Not divisible by 3 or 5");
 }
